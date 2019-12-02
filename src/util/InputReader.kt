3b066b9479b6ef19160cf779a8e6a445480ad6e2
Jente Sondervorst <jente.sondervorst@colruytgroup.com>
2019-12-02 23:53:42 +0100

    initial commit 2019

---
+++ src/util/InputReader.kt
@@ -0,0 +1,18 @@
+package util
+
+import java.io.File
+import java.util.stream.IntStream
+import java.util.stream.Stream
+
+
+fun readFileLineByLineToText(fileName: String): Stream<String> {
+    return File(ClassLoader.getSystemResource(fileName).file).readLines(Charsets.UTF_8).stream()
+}
+
+fun readFileLineByLineToInt(fileName: String): IntStream {
+    return readFileLineByLineToText(fileName).mapToInt { it.toInt() }
+}
+
+fun readFileLineCsvToInt(fileName: String): IntStream{
+    return File(ClassLoader.getSystemResource(fileName).file).readText(Charsets.UTF_8).split(",").stream().mapToInt { it.toInt() };
+}
